8bb386af90a94f51648a0f2e0e80444879f94baa
tb <tbit_144135@sina.com>
2025-06-10 13:42:07 +0800

    support firebase setting

--- flutter_firebase_login/android/app/build.gradle.kts
+++ flutter_firebase_login/android/app/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.flutter_firebase_login"
+    namespace = "com.tb.firebase"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
     compileSdk = 35
@@ -22,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_firebase_login"
+        applicationId = "com.tb.firebase"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
